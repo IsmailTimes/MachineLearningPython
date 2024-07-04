f4f142b8a036ad6e63b506b5a4dd57ba62f7b790
ISMAIL <mdismailhossain215@gmail.com>
2024-07-04 21:07:52 +0600

    Data file import

---
+++ Part 1- Data Preprocessing/Data Preprocessing Tools.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jul  4 20:42:37 2024
+
+@author: ISMAIL
+"""
+
+import pandas as pd
+import matplotlib.pylot as plt
+import numpy as np
+
+dataset = pd.read_csv("Data.csv")
+print(dataset)
+
